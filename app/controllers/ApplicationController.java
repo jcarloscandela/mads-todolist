8fc95edacb86050ca2dde1311fa99bb80a85f95d
jccandela <jccandelabordera@gmail.com>
2016-09-26 00:28:33 +0200

    Tic-3 modifico ficheros para que funcione JPA

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -6,8 +6,4 @@ public class ApplicationController extends Controller {
   public Result saludo(String nombre){
     return ok(saludo.render(nombre));
   }
-
-  public Result nuevo(){
-    return ok(nuevo.render());
-  }
 }
